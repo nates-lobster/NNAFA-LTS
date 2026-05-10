bbf1a53ed254daf4db8c317225e9332e5c69368d
Nate <nate@muse.local>
2026-05-10 18:23:32 -0400

    Fix activate script in bat files

--- run_emulator.bat
+++ run_emulator.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting NNAFA LSL Emulator...
-.\venv\Scripts\Activate.ps1 & python src/01_ingestion/emulator.py
+.\venv\Scripts\activate.bat & python src/01_ingestion/emulator.py
 pause
